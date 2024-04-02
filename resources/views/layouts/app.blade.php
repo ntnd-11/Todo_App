c9baf5f7790832b97c462588222e29e4205e5dcb
ntnd-11 <92575805+ntnd-11@users.noreply.github.com>
2024-04-02 13:46:26 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,19 +6,17 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            {{-- @include('layouts.navigation') --}}
+            <livewire:layout.navigation />
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -31,7 +29,7 @@
 
             <!-- Page Content -->
             <main>
-                @yield('main')
+                {{ $slot }}
             </main>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', function () {
+Route::get('/', function(){
     $tasks = Task::orderBy('id','asc')
-    ->paginate(3);
+    ->paginate((2));
     return view('taskList', compact('tasks'));
 });
 
@@ -18,28 +14,25 @@ Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.crea
 Route::post('/tasks/store', [TaskController::class, 'store'])->name('tasks.store');
 Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
 Route::put('/tasks/{task}/update', [TaskController::class, 'update'])->name('tasks.update');
-Route::get('/tasks/{task}/delete', [TaskController::class, 'delete'])->name('tasks.delete');
 Route::get('/tasks/{task}/restore', [TaskController::class, 'restore'])->name('tasks.restore');
+Route::get('/tasks/{task}/delete', [TaskController::class, 'delete'])->name('tasks.delete');
 Route::get('/tasks/{task}/destroy', [TaskController::class, 'destroy'])->name('tasks.destroy');
-
+Route::get('/tasks/search', [TaskController::class, 'search'])->name('tasks.search');
 
 Route::get('/tasks/rb', function(){
     $tasks = Task::onlyTrashed()
     ->orderBy('id','asc')
-    ->paginate(3);
+    ->paginate((2));
     return view('taskRB', compact('tasks'));
 })->name('tasks.rb');
 
-Route::get('tasks/search', [TaskController::class, 'search'])->name('tasks.search');
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
